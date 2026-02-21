b45df1807315f3c988bfacb9a3c224e4d895bb4b
joseph <josephsf221@gmail.com>
2026-02-20 21:16:39 -0700

    finished all web app features, working on adding functionality to remaining rank columns, completion soon... :)

--- backend/src/main/java/com/mazetube/rand_vid_finder/services/YoutubeAPI.java
+++ backend/src/main/java/com/mazetube/rand_vid_finder/services/YoutubeAPI.java
@@ -6,7 +6,7 @@ import com.mazetube.rand_vid_finder.WordBank;
 
 @Service            
 public class YoutubeAPI {
-    private static final String API_KEY = "AIzaSyArljrOKIaxUEaiJ_G7N1sNIGaexPSzhok";
+    private static final String API_KEY = "api key here";
 
     public static String fetchRandVideoID(){
          String response = "";
